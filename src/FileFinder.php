239a90e944148fbc22deec7f17dbaa1ebdecf6f6
Greg Bowler <greg.bowler@g105b.com>
2016-02-16 15:56:03 +0000

    Add FileFinder class for #10

---
+++ src/FileFinder.php
@@ -0,0 +1,14 @@
+<?php
+namespace phpgt\config;
+
+/**
+ * The FileFinder searches the provided path for config files that match a
+ * criteria, in case a specific file path isn't provided.
+ */
+class FileFinder {
+
+public function __construct() {
+
+}
+
+}#
