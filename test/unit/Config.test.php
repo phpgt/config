865c661f5cac65ee0e77b61e896a08c970833970
Greg Bowler <greg.bowler@g105b.com>
2016-02-16 21:44:47 +0000

    TDD for prefixes

--- test/unit/Config.test.php
+++ test/unit/Config.test.php
@@ -16,15 +16,29 @@ public function tearDown() {
 	test\Helper::removeTmpDir();
 }
 
-public function testIniWithPath() {
+public function testIni() {
 	$this->filePath = test\Helper::createConfigFile(
 		$this->tmp, Config::TYPE_INI);
 	$baseName = pathinfo($this->filePath, PATHINFO_BASENAME);
 
+	$this->assertEmpty(getenv("key"));
 	$this->assertEmpty(getenv("another_key"));
 	$config = new Config(false, $baseName, $this->tmp);
 
+	$this->assertEquals("value", getenv("key"));
 	$this->assertEquals("another value", getenv("another_key"));
+}
+
+public function testPrefix() {
+	$this->filePath = test\Helper::createConfigFile(
+		$this->tmp, Config::TYPE_INI);
+	$baseName = pathinfo($this->filePath, PATHINFO_BASENAME);
+
+	$config = new Config(false, $baseName, $this->tmp);
+	$config->setPrefix("testprefix");
+
+	$this->assertEquals("value", getenv("testprefix.key"));
+	$this->assertEquals("another value", getenv("testprefix.another_key"));
 }
 
 }#
