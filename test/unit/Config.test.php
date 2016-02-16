b6fad146ac2fc24f639883b6d2b9e4eeaa9696f4
Greg Bowler <greg.bowler@g105b.com>
2016-02-16 18:10:36 +0000

    Test flat ini parsing

--- test/unit/Config.test.php
+++ test/unit/Config.test.php
@@ -3,8 +3,28 @@ namespace phpgt\config;
 
 class ConfigTest extends \PHPUnit_Framework_TestCase {
 
-public function testTest() {
-	$this->assertTrue(true);
+private $tmp;
+
+public function setUp() {
+	test\Helper::setUpTmpDir();
+
+	$dir = test\Helper::getTmpDir();
+	$this->tmp = $dir;
+}
+
+public function tearDown() {
+	test\Helper::removeTmpDir();
+}
+
+public function testIniWithPath() {
+	$this->filePath = test\Helper::createConfigFile(
+		$this->tmp, Config::TYPE_INI);
+	$baseName = pathinfo($this->filePath, PATHINFO_BASENAME);
+
+	$this->assertEmpty(getenv("another_key"));
+	$config = new Config(false, $baseName, $this->tmp);
+
+	$this->assertEquals("another value", getenv("another_key"));
 }
 
 }#
