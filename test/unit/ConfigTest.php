fd8b10bff7046370d9d29b12dfc2ee58c1653d85
Greg Bowler <greg.bowler@g105b.com>
2018-01-29 22:46:26 +0000

    Test ENV variable

--- test/unit/ConfigTest.php
+++ test/unit/ConfigTest.php
@@ -2,11 +2,26 @@
 namespace Gt\Config\Test;
 
 use Gt\Config\Config;
+use Gt\Config\Test\Helper\Helper;
 use PHPUnit\Framework\TestCase;
 
 class ConfigTest extends TestCase {
+	public function setUp() {
+		Helper::removeTmpDir();
+		mkdir(Helper::getTmpDir(), 0775, true);
+	}
+
+	public function tearDown() {
+		Helper::removeTmpDir();
+	}
+
 	public function testEnvVarNotPresentByDefault() {
 		$config = new Config([], "");
 		$this->assertNull($config->get("OS"));
+	}
+
+	public function testEnvVarPresentWithEnv() {
+		$config = new Config($_ENV, "");
+		$this->assertNotNull($config->get("OS"));
 	}
 }
